5b01d14cb38c97eb836b2c60b0f95b1c2e1d475f
Touko Toivonen <touko.t.toivonen@gmail.com>
2026-01-26 15:32:22 +0200

    chore: try fixing routing problem, backend-route wip

--- frontend/src/services/pingService.ts
+++ frontend/src/services/pingService.ts
@@ -1,7 +1,5 @@
-const backendUrl = import.meta.env.VITE_BACKEND_URL || "https://ohtuprojekti-staging.svc.cluster.local:3000";
-
 async function pingServer(): Promise<string> {
-  const response = await fetch(`${backendUrl}/ping`, {
+  const response = await fetch("http://sijoittaja-backend-svc/ping", {
     method: "GET",
   });
 
